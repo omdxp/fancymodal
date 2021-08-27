78f18dfaf17f96e3b61b46d8f3b0a5090214885c
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-08-28 00:18:27 +0100

    using modalbpx package

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import {View, Text, Button} from 'react-native';
-import Modal from 'react-native-modal';
+import Modal from 'react-native-modalbox';
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Button title="open modal" onPress={() => setIsModalVisible(true)} />
-      <Modal isVisible={isModalVisible}>
-        <View style={{flex: 1}}>
-          <Text>Hello World</Text>
+      <Modal isOpen={isModalVisible}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text>Hello World!</Text>
         </View>
       </Modal>
     </View>
