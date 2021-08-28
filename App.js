68acb05605bf1ed5c119624929fd4c6bf361c469
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-08-28 01:11:12 +0100

    testing simple modal

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React, {useState} from 'react';
 import {View, Text, Button} from 'react-native';
-import {AnimatedModal} from 'react-native-root-modal';
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Button title="open modal" onPress={() => setIsModalVisible(true)} />
-      <AnimatedModal visible={isModalVisible}>
-        <View style={{backgroundColor: '#fff', padding: 20}}>
-          <Text>Hello World!</Text>
-          <Button
-            title="close modal"
-            onPress={() => setIsModalVisible(false)}
-          />
-        </View>
-      </AnimatedModal>
     </View>
   );
 }
